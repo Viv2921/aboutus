f58b512387f8d521627047c2c700340cd761c58a
Vivaswan <vkurwatti@gmail.com>
2023-05-17 22:07:16 +0530

    4th commit

--- views/script.js
+++ views/script.js
@@ -1,10 +1,14 @@
-const el = document.querySelector("#circle");
-
+const el = document.querySelector("#element");
+const circle = document.querySelector("#circle");
 
 el.addEventListener("mousemove", (e) => {
-  el.style.backgroundPositionX = -e.offsetX + "px";
-  el.style.backgroundPositionY = -e.offsetY + "px";
+  circle.style.backgroundPositionX = -e.clientX + "px";
+  
+  el.style.backgroundPositionX = -e.clientX + "px";
+ 
   console.log("X :", el.style.backgroundPositionX)
-  console.log( "y:", el.style.backgroundPositionY)
+  circle.style.backgroundPositionX= circle.style.backgroundPositionX/3 +'px 0';
+  el.style.backgroundPositionX =el.style.backgroundPositionX/3 + 'px 0'
+ 
+  
 });
-
