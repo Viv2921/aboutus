df1682c3e030ae1dac355d2b344c22f366a9f305
Vivaswan <vkurwatti@gmail.com>
2023-05-19 16:50:15 +0530

    rotating heads

--- views/script.js
+++ views/script.js
@@ -2,43 +2,84 @@ const el = document.querySelector("#element");
 const circle = document.querySelector("#circle");
 
 el.addEventListener("mousemove", (e) => {
-  console.log("Mouse pos x, y", e.clientX-280, 265-e.clientY );
-  let x = e.clientX-337
-  let y = 245-e.clientY
-  let angle = 0;
-  if (x < 0) {
-    angle = 270 - (Math.atan(y / -x) * 180 / Math.PI);
-} else {
-    angle = 90 + (Math.atan(y / x) * 180 / Math.PI);
-}
-console.log("Angle = ", angle)
-
-  if(angle > 0 && angle < 45){
-  circle.style.backgroundPositionX = '-630px';
-}
-else if(angle >=45 && angle < 90){
-  circle.style.backgroundPositionX = '-277px';
-}
-else if(angle >=90 && angle < 135){
-  circle.style.backgroundPositionX = "-360px"
-}
-else if(angle >=135 && angle < 180 ){
-  circle.style.backgroundPositionX="-445px"
-}
-else if(angle>180 && angle <230){
-  circle.style.backgroundPositionX="-535px"
-}
-else if(angle>=230 && angle <300){
-  circle.style.backgroundPositionX="-873px"
-}
-else if(angle>=300 && angle < 340){
-  circle.style.backgroundPositionX="-780px"
-}
-
-else if(angle>=340 && angle <360){
-  circle.style.backgroundPositionX="-705px"
-}
-
+  console.log("Mouse pos x, y", e.clientX, e.clientY);
+  let mousePosX = e.clientX
+  let mousePosY = e.clientY
+  for (let i =0; i < 105; i++){
+    let ele = document.getElementsByClassName(i)[0];
+    let centerX = ele.offsetLeft + ele.offsetWidth / 2;
+    let centerY = ele.offsetTop + ele.offsetHeight / 2;
+    console.log(`image ${i}`,centerX, centerY , mousePosX, mousePosY)
+  //   let angle = getAngleDegree(centerX, centerY, mousePosX, mousePosY)
+  // console.log("Angle = ", angle)
+  
+  ele.style.backgroundPositionX = getImageXpostion(centerX, centerY, mousePosX, mousePosY);
+  }
   
 });
+
+function getAngleDegree(originX, originY, targetX, targetY){
+ let angle =  Math.atan((targetY - originY)/(targetX - originX))
+ return angle
+}
+
+function getImageXpostion(centerX, centerY, mousePosX, mousePosY){
+  let posX = 0;
+  if(centerX === mousePosX && mousePosY > centerY){
+    console.log("bottom")
+    posX = -700
+  } else if (centerX === mousePosX && mousePosY < centerY){
+    console.log("up")
+    posX = -443;
+  } else if (mousePosY === centerY && centerX < mousePosX){
+    console.log("right")
+    posX = -277
+  } else if (mousePosY === centerY && centerX > mousePosX){
+    console.log("left")
+
+    posX = -870
+  } else if(mousePosX > centerX && mousePosY > centerY){
+    console.log("bottom left")
+
+    posX = -630
+    
+  } else if (mousePosX < centerX && mousePosY > centerY){
+    console.log("bottom right")
+
+    posX = -780
+  } else if (mousePosX < centerX && mousePosY < centerY){
+    console.log("top left")
+    posX = -535
+    
+  } else if(mousePosX > centerX && mousePosY < centerY){
+    console.log("top rioght")
+    posX = -360
+  }
+  // let posX = 0
+  // if(angle > 0 && angle < 45){
+  //   posX = -630;
+  // }
+  // else if(angle >=45 && angle < 90){
+  //   posX = -277
+  // }
+  // else if(angle >=90 && angle < 135){
+  //   posX = -360
+  // }
+  // else if(angle >=135 && angle < 180 ){
+  //   posX = -445
+  // }
+  // else if(angle>180 && angle <230){
+  //   posX = -535
+  // }
+  // else if(angle>=230 && angle <300){
+  //   posX = -873
+  // }
+  // else if(angle>=300 && angle < 340){
+  //   posX = -780
+  // }
+  // else if(angle>=340 && angle <360){
+  //   posX = -705
+  // }
+  return posX+"px"
+}
 
